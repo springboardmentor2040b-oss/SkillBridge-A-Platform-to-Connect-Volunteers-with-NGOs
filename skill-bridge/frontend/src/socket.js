4367d3dabf7dc070039719fcaa7adf58a82d0f4c
springboardmentor2040b-oss <springboardmentor2040b@gmail.com>
2026-01-20 19:56:57 +0530

    Delete skill-bridge directory

--- skill-bridge/frontend/src/socket.js
+++
@@ -1,7 +0,0 @@
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:5000", {
-  transports: ["websocket", "polling"]
-});
-
-export default socket;
--- skill-bridge/src/main.js
+++
@@ -1,7 +0,0 @@
-import ReactDOM from "react-dom/client";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1 style={{ color: "black", fontSize: "40px" }}>
-    HELLO FROM MAIN.JSX
-  </h1>
-);
