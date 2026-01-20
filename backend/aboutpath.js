9ca4709ff322f01607ea27b3e21db60a42ec1076
Krrish0001-dot <krrishlather666@gmail.com>
2026-01-20 23:58:11 +0530

    Added Team 3 frontend and backend

---
+++ backend/aboutpath.js
@@ -0,0 +1,9 @@
+import path from "path"
+
+let myPath = "c:\\backend\\aboutpath.js"
+
+console.log(path.extname(myPath))
+console.log(path.dirname(myPath))
+console.log(path.basename(myPath))
+
+console.log(path.join("c:/","programs\\harry.txt"))
---
+++ backend/mainpromise.js
@@ -0,0 +1,7 @@
+import fs from "fs/promises"
+
+let a = await fs.readFile("harry.txt")
+
+let b = await fs.appendFile("harry.txt","\n\nThis is amazing promise")
+
+console.log(a.toString(),b)
